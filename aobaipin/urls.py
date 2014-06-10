00c12f6447da2629a224cc49380e46d9b7fcca0a
dongtaoy <dongtao.yu2010@gmail.com>
2014-06-10 22:45:30 +1000

    initialize

--- aobaipin/urls.py
+++ aobaipin/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-
+import aobaipin
+from aobaipin.views import world
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'aobaipin.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'aobaipin.views.hello'),
+    url(r'^world/', world),
     url(r'^admin/', include(admin.site.urls)),
 )
