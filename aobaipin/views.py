00e6a4b5b201b88ec864fe5c5adf89a9a90498bc
dongtaoy <dongtao.yu2010@gmail.com>
2014-06-10 23:32:49 +1000

    initialize

--- aobaipin/views.py
+++ aobaipin/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
 
 def hello(request):
-    return HttpResponse("Hello")
+    t = get_template("base.html")
+    html = t.render(Context())
+    return HttpResponse(html)
 
 def world(request):
     return HttpResponse("World")
